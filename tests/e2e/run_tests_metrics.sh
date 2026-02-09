d5fd330878fc8b3378ee38d6a7a7112d0febf891
Vladislav Klimenko <sunsingerus@gmail.com>
2026-02-09 13:26:05 +0500

    test: metrics

--- tests/e2e/run_tests_metrics.sh
+++ tests/e2e/run_tests_metrics.sh
@@ -6,5 +6,12 @@ export OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-"test"}"
 export OPERATOR_INSTALL="${OPERATOR_INSTALL:-"yes"}"
 export IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-"Always"}"
 
+RUN_ALL="${RUN_ALL:-""}"
 ONLY="${ONLY:-"*"}"
-python3 "$CUR_DIR/../regression.py" --only="/regression/e2e.test_metrics_exporter/${ONLY}" --native
+
+# We may want run all tests to the end ignoring failed tests in the process
+if [[ ! -z "${RUN_ALL}" ]]; then
+    RUN_ALL="--test-to-end"
+fi
+
+python3 "$CUR_DIR/../regression.py" --only="/regression/e2e.test_metrics_exporter/${ONLY}" ${RUN_ALL} --parallel off -o short --native
